f859d754f0575e44f37d760d27816ebff3b99e65
unknown <mtharwat@onesolutionc.com>
2023-10-26 19:32:56 +0300

    add drf_spectacular and swagger for document my api

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='accounts/import')),
     path('', include("account.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
