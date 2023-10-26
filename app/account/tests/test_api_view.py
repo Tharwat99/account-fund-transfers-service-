bd326f0a21987c2541800f06298714d7bd286a34
unknown <mtharwat@onesolutionc.com>
2023-10-26 16:46:56 +0300

    add list_accounts_api test cases

--- app/account/tests/test_api_view.py
+++ app/account/tests/test_api_view.py
@@ -1,10 +1,11 @@
 import os 
+import uuid
 from rest_framework.test import APITestCase
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from ..models import Account  
-from ..serializers import ImportAccountsSerializer
+from ..serializers import AccountSerializer 
 
 
 class ApiViewTestCase(APITestCase):
@@ -34,3 +35,16 @@ class ApiViewTestCase(APITestCase):
         response = self.client.post(url, data, format='multipart')
     
         self.assertEqual(response.status_code, 400)
+
+    def test_account_list_view_success(self):
+        Account.objects.create(id = uuid.uuid4(), name='Account1', balance=100.0)
+        Account.objects.create(id = uuid.uuid4(), name='Account2', balance=50.0)
+        url = reverse('list_accounts_api')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 2)
+
+        serialized_data = AccountSerializer(Account.objects.all().order_by('-id'), many=True).data
+        self.assertEqual(response.data['results'], serialized_data)
